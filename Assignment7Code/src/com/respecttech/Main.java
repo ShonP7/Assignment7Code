2e0159a8b92ad1146c71dbe4a453e4c7c2818085
Shon Pierce <Shon Pierce@DESKTOP-2AFJFDJ.mshome.net>
2021-10-17 16:39:53 -0400

    First commit....

---
+++ Assignment7Code/src/com/respecttech/Main.java
@@ -0,0 +1,36 @@
+package com.respecttech;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		int myValue = 10000;
+		int myMinValue = Integer.MIN_VALUE;
+		int myMaxValue = Integer.MAX_VALUE; 
+		System.out.println("Interger Minimum Value = " + myMinValue);
+		System.out.println("Interger Maximum Value = " + myMaxValue);
+		
+		byte myMinByteValue = Byte.MIN_VALUE;
+		byte myMaxByteValue = Byte.MAX_VALUE;
+		System.out.println("\nByte Minimum Value = " + myMinByteValue);
+		System.out.println("Byte Maximum Value = " + myMaxByteValue);
+		
+		short myMinShortValue = Short.MIN_VALUE;
+		short myMaxShortValue = Short.MAX_VALUE;
+		System.out.println("\nShort Minumun Value = " + myMinShortValue);
+		System.out.println("Short Maximun Value = " + myMaxShortValue);
+		
+		
+		long myLongValue = 100L;
+		long myMinLongValue = Long.MIN_VALUE;
+		long myMaxLongValue = Long.MAX_VALUE;
+		System.out.println("\nLong Minumun Value = " + myMinLongValue);
+		System.out.println("Long Maximun Value = " + myMaxLongValue);
+		
+		
+		
+	}
+
+}
+
+ 
